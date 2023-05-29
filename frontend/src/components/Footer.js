6bf0ce9cf61378f72b96e2f03dc0fbeef5161f3b
maevezhw <maeve.zahwa.adriana.crown.zaki@mail.ugm.ac.id>
2023-05-29 15:25:13 +0700

    feat: footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,58 +1,105 @@
 import React from "react";
-// import circle from "/img/landing-page/Ellipse 3.png";
-// import wave from "/img/landing-page/footer.png";
-// import logo from "/img/landing-page/Group 1.png";
-// import wave2 from "/img/landing-page/Rectangle 29.png";
 import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <footer>
-      <img className="wave-footer" src="/img/landing-page/Rectangle 29.png" />
-      <img className="logo-circle" src="/img/landing-page/Ellipse 3.png" />
-      <img className="logo-shortened" src="/img/landing-page/Group 1.png" />
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <h4>About Us</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac
-              ante at ante vehicula hendrerit sed non magna.
-            </p>
+          <div className="row-1">
+            <div className="col1">
+              <img className = "footer-logo" src="img/ecommerce/logo-blue.svg"/>
+            </div>
+            <div className="col2">
+              <p>Help you get the look that you always dream of</p>
+            </div>
+            {/* <div className="col3 list-socmed">
+              <a href="#">
+                <i className="fa fa-facebook"></i>
+              </a>
+            </div> */}
           </div>
-          <div className="col-md-4">
-            <h4>Contact Us</h4>
-            <ul className="list-unstyled">
-              <li>
-                <i className="fa fa-map-marker"></i> 123 Main Street, New York,
-                NY 10001
-              </li>
-              <li>
-                <i className="fa fa-phone"></i> (123) 456-7890
-              </li>
-              <li>
-                <i className="fa fa-envelope"></i> info@example.com
-              </li>
-            </ul>
+          <div className="row-2">
+            <div className="col-1">
+              <h4 className="subtitle">Information</h4>
+              <ul className="list-unstyled information">
+                <li>
+                  <a href="#">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Terms & Conditions
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    FAQ
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Help Center
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-2">
+              <h4 className="subtitle">Shop by Category</h4>
+              <ul className="list-unstyled information">
+                <li>
+                  <a href="#">
+                    Perfumes
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Cosmetics
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Clothes
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-3">
+              <h4 className="subtitle">Shop by Brands</h4>
+              <ul className="list-unstyled information">
+                <li>
+                  <a href="#">
+                    Effe
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Lumiere
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Zalya
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="col-md-4">
-            <h4>Follow Us</h4>
-            <ul className="list-unstyled">
-              <li>
-                <a href="#">
-                  <i className="fa fa-facebook"></i> Facebook
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i className="fa fa-twitter"></i> Twitter
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i className="fa fa-instagram"></i> Instagram
-                </a>
-              </li>
+          <div className="row-3">
+            <p>Made with ♡ by Pluto team</p>
+            <ul className="member-team">
+              <li>Nasywa</li>
+              <li>Maeve</li>
+              <li>Safira</li>
+              <li>Venus</li>
+              <li>Coveeta</li>
+              <li>Vincent</li>
             </ul>
           </div>
         </div>
